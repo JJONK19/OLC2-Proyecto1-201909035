61892b64fc27b3c1c87bb321f1a517f393fe2080
JJONK19 <josuejaviera19@gmail.com>
2022-07-30 23:10:43 -0600

    Creacion del Editor

--- Codigo Fuente/Main.py
+++ Codigo Fuente/Main.py
@@ -1,9 +1,10 @@
 from PyQt5 import QtWidgets, uic, QtCore
 from PyQt5.QtWidgets import QTableWidgetItem
 import sys
-import os.path
+from PyQt5.QtGui import *
 import os
 from PyQt5.QtGui import QPixmap
+from PyQt5.Qsci import QsciScintilla, QsciLexerPython
 
 class ventana (QtWidgets.QMainWindow):
     
@@ -16,8 +17,34 @@ class ventana (QtWidgets.QMainWindow):
         self.BBases.clicked.connect(lambda: self.Vistas.setCurrentIndex(3))
         self.BTablas.clicked.connect(lambda: self.Vistas.setCurrentIndex(4))
         self.BInfo.clicked.connect(lambda: self.Vistas.setCurrentIndex(5))
-        self.show()
+        self.BAnalizar.clicked.connect(self.analizar)
+        self.BLimpiar.clicked.connect(self.limpiar)
 
+        #Manejo de los editores
+        #Basado en https://www.faqcode4u.com/faq/124137/qscintilla-based-text-editor-in-pyqt5-with-clickable-functions-and-variables
+        font = QFont()
+        font.setPointSize(12)
+        font.setFamily('Courier')
+        font.setFixedPitch(True)
+        metrics = QFontMetrics(font)
+        self.Consola.setReadOnly(True)
+        self.Consola.setFont(font)
+        self.Consola.setMarginsFont(font)
+        self.Entrada.setFont(font)
+        self.Entrada.setMarginsFont(font)
+        self.Consola.setMarginsBackgroundColor(QColor("#395b64"))
+        self.Entrada.setMarginsBackgroundColor(QColor("#395b64"))
+        self.Entrada.setMarginsFont(font)
+        self.Consola.setMarginsFont(font)
+        self.Consola.setMarginLineNumbers(0, True)
+        self.Entrada.setMarginLineNumbers(0, True)
+        self.Entrada.setMarginWidth(0, metrics.width("00000") + 3)
+        self.Consola.setMarginWidth(0, metrics.width("00000") + 3)
+
+        #Variables de la aplicación
+        ENTRADA = ""                #Texto ubicado en el espacio de entrada
+        SALIDA = ""                 #Texto a colocar en la consola
+        self.show()
 
     def añadirFilas(self, info, tabla):
         """
@@ -42,6 +69,24 @@ class ventana (QtWidgets.QMainWindow):
 
         """
         tabla.setRowCount(0)
+
+    def analizar(self):
+        """
+        analizar toma el texto ubicado en Entrada y lo manda a analizar. Este metodo prepara
+        y distribuye la información por toda la aplicación.
+
+        """
+        #--- Recopilar el texto
+        ENTRADA = self.Entrada.text()
+
+    def limpiar(self):
+        """
+        limpiar borra toda la información almacenada en la consola y en las tablas.
+
+        """
+        self.Entrada.setText("")
+        self.Consola.setText("")
+        
 
 app = QtWidgets.QApplication(sys.argv)
 main = ventana()
